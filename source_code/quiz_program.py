eaf33f233f8f13266e993d67ad53f37376297f50
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-05-04 23:56:41 +0800

    Imported the dependencies to use in the program

---
+++ source_code/quiz_program.py
@@ -0,0 +1,8 @@
+# General Instructions: Create the Quiz program that read the output file of the Quiz Creator; 
+# The user will answer the randomly selected question and check if the answer is correct.
+
+# Install library to use
+
+# Import library
+import pygame
+import sys
