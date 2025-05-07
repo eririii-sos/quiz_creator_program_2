34aa366c9664d94983b0e7eab525db8d30f69081
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-05-08 02:27:40 +0800

    Added window title and allowed display to be constantly updated

--- source_code/quiz_program.py
+++ source_code/quiz_program.py
@@ -15,6 +15,7 @@ pygame.mixer.init()
 WIDTH, HEIGHT = 1067, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 screen.fill((255, 255, 255))  # Initial background fill
+pygame.display.set_caption("Quiz Day")
 
 # Main loop
 running = True
@@ -22,3 +23,6 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # Update the display
+    pygame.display.flip()            
